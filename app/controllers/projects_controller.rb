585d3494b7f281f847f88a67cdfe6277b64d85eb
samgrimm <samantha.grimm@gmail.com>
2017-01-17 18:58:12 -0600

    added the new project path, nav bar to application

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,15 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.projects_by(current_user)
   end
+
+  def new
+    @project = Project.new
+  end
+
+  private
+
+
+  def project_params
+    params.require(:project).permit(:name, :start_date)
+  end
 end
