e7a382b8cc3f983c7bd9409fc7c3cf4a358aaee0
samgrimm <samantha.grimm@gmail.com>
2017-01-25 13:14:38 -0600

    added a separate layout for the static pages and fixed the navbar for the dynamic content

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   devise_for :users
+  authenticated :user do
+    root 'projects#index', as: "authenticated_root"
+  end
   root to: "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
