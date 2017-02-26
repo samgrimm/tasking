d9b9d8afea13ada25d8ce25dbb25cafde4a93a52
samgrimm <samantha.grimm@gmail.com>
2017-02-25 19:41:13 -0600

    added availability to the permitted params for sign up with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_signup_parameters, if: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:availability])
+  end
+
+  def configure_signup_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:availability])
   end
 end
